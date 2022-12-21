f93a3eddc39ebefb950ead2ec39de49c7f55e0d5
Antonios Marinos <antoniosmarinos7@gmail.com>
2022-12-21 21:35:15 +0100

    added map to routing_app

--- routing_app.py
+++ routing_app.py
@@ -1,5 +1,6 @@
 from or_tools.travelling_salesman_problem import optimize_routes
 from distance_api.distance_matrix import create_distance_matrix
+from distance_api.geocoding import visualize_map
 from constants import api_key
 
 # Enter addresses
@@ -21,4 +22,8 @@ class RoutingOptimizer:
 
     def optimize_routes_tsp(self,):
         results = optimize_routes(self.distance_matrix, self.depot)
+        return results
+
+    def create_map(self,):
+        results = visualize_map(self.addresses)
         return results
