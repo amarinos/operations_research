08b6cd6feba4d9b1a085e8e7817d3a187ba3380f
Antonios Marinos <antoniosmarinos7@gmail.com>
2022-12-22 07:25:47 +0100

    Add documentation to routing class step 1

--- routing_app.py
+++ routing_app.py
@@ -3,8 +3,16 @@ from distance_api.distance_matrix import create_distance_matrix
 from distance_api.geocoding import visualize_map
 from constants import api_key
 
-# Enter addresses
 class RoutingOptimizer:
+    """
+    This class enables access to the google distance API and the google OR-tools
+    You can provide a set of addresses, get the distance matrix between them and then calculate
+    the optimal routing between them (travelling salesman problem)
+
+    Args:
+        addresses: Define a list of addresses when initializing (not mandatory)
+        depot: Define the integer position in the list, where your depot (starting point) is
+    """
     def __init__(self,addresses = [],depot= 0, api_key = api_key):
         self.addresses = addresses
         self.depot = depot
@@ -12,13 +20,23 @@ class RoutingOptimizer:
         self.distance_matrix = None
 
     def add_address(self,address):
+        """
+        Add an address to the addresses list
+        """
         self.addresses.append(address)
 
     def remove_address(self,address):
+        """
+        Remove an address from the addresses list
+        """
         self.addresses.remove(address)
 
     def create_distance_matrix(self):
-            self.distance_matrix = create_distance_matrix(self.addresses, self.api_key)
+        """
+        Call the google distance API for the defined addresses parameter
+        Calculates the distance_matrix and saves it within the class
+        """
+        self.distance_matrix = create_distance_matrix(self.addresses, self.api_key)
 
     def optimize_routes_tsp(self,):
         results = optimize_routes(self.distance_matrix, self.depot)
